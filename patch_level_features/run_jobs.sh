65b8ab4da1e9636c1f88ecb94911a25d7a03e0fc
bridge wang <tigerfan7495@gmail.com>
2018-05-30 16:06:59 -0400

    Update run_jobs.sh

--- patch_level_features/run_jobs.sh
+++ patch_level_features/run_jobs.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
-input_file="case_id_list_2017_12_12"
-IFS=$'\n' read -d '' -r -a caseid_list < $input_file
-
-for case_id in "${caseid_list[@]}"
+echo "Please enter image_user_list filename:"
+read filename
+while IFS='' read -r line || [[ -n "$line" ]]; 
 do  
-  qsub -v caseid=$case_id run_jobs.pbs   
-done
+  IFS=', ' read -r -a array <<< "$line"  
+  case_id="${array[0]}"
+  user="${array[1]}"
+  #echo $case_id, $user
+  qsub -v caseid=$case_id,user=$user run_jobs.pbs  
+done < $filename
